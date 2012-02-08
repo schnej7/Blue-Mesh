07f993b43911e81d651492792ccd6e243e499bb5
schnej7 <schnej7@rpi.edu>
2012-02-08 17:09:06 -0500

    Added function skeletons

--- BlueMesh/src/blue/mesh/BlueMeshService.java
+++ BlueMesh/src/blue/mesh/BlueMeshService.java
@@ -4,6 +4,33 @@ package blue.mesh;
 
 public class BlueMeshService {
 	
+	public BlueMeshService(){
+		
+	}
 	
+	public int config(){
+		
+		return Constants.SUCCESS;
+	}
+	
+	public int launch(){
+		
+		return Constants.SUCCESS;
+	}
+	
+	public int write( byte [] buffer){
+		
+		return Constants.SUCCESS;
+	}
+	
+	public byte [] pull(){
+		
+		return null;
+	}
+	
+	public int disconnect(){
+		
+		return Constants.SUCCESS;
+	}
 
 }
