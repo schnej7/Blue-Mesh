e59aa54fa8d1be5e80f3ff672ea388568b8b1f58
unknown <The Omnissiah@TheOmnissiah-PC.(none)>
2012-02-07 21:10:10 -0500

    wrote the run() function for ClientThread

--- BlueMesh/src/blue/mesh/ClientThread.java
+++ BlueMesh/src/blue/mesh/ClientThread.java
@@ -1,10 +1,18 @@
 package blue.mesh;
+import java.io.IOException;
+import java.util.Set;
+
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.os.Handler;
+import android.os.ParcelUuid;
+import android.util.Log;
 
 
 
 public class ClientThread {
+	private static final String TAG = "BluetoothChatService";
     private Handler handler;
     private BluetoothAdapter adapter;
     private RouterObject routerObject;
@@ -17,10 +25,39 @@ public class ClientThread {
 	}
 	
 	void run() {
-		//Using variables in code to get rid of warnings
-		handler.notify();
-		adapter.enable();
-		routerObject.notify();
+		
+		//get list of all paired devices
+		Set <BluetoothDevice> pairedDevices = adapter.getBondedDevices();
+		
+		for (BluetoothDevice d : pairedDevices)
+		{
+			//for each paired device, get uuids
+			ParcelUuid[] uuids = d.getUuids();
+			
+			for (ParcelUuid u : uuids)
+			{
+				//for each uuid, try to open a socket on it
+				BluetoothSocket clientSocket = null;
+				try {
+					 clientSocket = d.createRfcommSocketToServiceRecord(u.getUuid());
+				}
+			
+				catch (IOException e) {
+				Log.e(TAG, "Socket create() failed", e);
+				}
+				
+				//once a socet is opened, try to connect and then pass to router
+				try {
+					clientSocket.connect();
+					routerObject.BeginConnection(clientSocket);
+				}
+				
+				catch (IOException e) {
+					Log.e(TAG, "Socket connect() failed", e);
+				}
+				
+			}
+		}
 		return;
 	}
 };
