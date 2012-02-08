8d361fb6c00ccb8f909f86d7a2b6896305b068c8
schnej7 <schnej7@rpi.edu>
2012-02-08 17:58:43 -0500

    Implimented Begin Connection and changed public to protected

--- BlueMesh/src/blue/mesh/RouterObject.java
+++ BlueMesh/src/blue/mesh/RouterObject.java
@@ -1,44 +1,72 @@
 package blue.mesh;
 
+import java.io.IOException;
+import java.util.List;
+
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.os.Handler;
+import android.util.Log;
 
 
 public class RouterObject {
 
 	private Handler handler;
 	private BluetoothAdapter adapter;
+	private List <String> connectedDevices;
+	private List <ReadWriteThread> rwThreads;
+	private final String TAG = "RouterObject";
 	
-	public RouterObject( Handler mHandler, BluetoothAdapter mAdapter) {
+	protected RouterObject( Handler mHandler, BluetoothAdapter mAdapter) {
 		handler = mHandler;
 		adapter= mAdapter;
 	}
 	
-	//TODO: MAKE THIS CALL SYNCHRONIZED
-	public int beginConnection(BluetoothSocket mSocket) {
+	protected synchronized int beginConnection(BluetoothSocket socket) {
 		
-		//Great Success!
-		return 1;
+		//Don't let another thread touch connectedDevices while
+		//I read and write it
+		synchronized( this.connectedDevices ){
+			//Check if the device is already connected to
+			if( connectedDevices.contains(socket.getRemoteDevice().getName())){
+				try {
+					socket.close();
+				} catch (IOException e) {
+					Log.e(TAG, "could not close() socket", e);
+				}
+				return Constants.SUCCESS;
+			}
+			//Add device name to list of connected devices
+			connectedDevices.add(socket.getRemoteDevice().getName());
+		}
+		
+		//Don't let another thread touch rwThreads while I add to it
+		ReadWriteThread aReadWriteThread = new ReadWriteThread(this, socket);
+		aReadWriteThread.start();
+		synchronized( this.rwThreads ){
+			rwThreads.add(aReadWriteThread);
+		}
+		
+		return Constants.SUCCESS;
 	}
 	
-	public int route(byte buff[]) {
+	protected int route(byte buff[]) {
 		//Great Success!
 		return Constants.SUCCESS;
 	}
 	
-	public byte [] getNextMessage() {
+	protected byte [] getNextMessage() {
 		byte arr[] = null;
 		return arr;
 	}
 	
-	int getDeviceState( BluetoothDevice device ){
+	protected int getDeviceState( BluetoothDevice device ){
 		
 		return Constants.STATE_NONE;
 	}
 	
-	public int stop() {
+	protected int stop() {
 		//Great Success!
 		return Constants.SUCCESS;
 	}
