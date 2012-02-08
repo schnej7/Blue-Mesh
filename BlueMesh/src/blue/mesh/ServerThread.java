4f9eee976885828caac98b88f126c60cdaac5e14
schnej7 <schnej7@rpi.edu>
2012-02-08 13:03:08 -0500

    Implimented ServerThread constructor

--- BlueMesh/src/blue/mesh/ServerThread.java
+++ BlueMesh/src/blue/mesh/ServerThread.java
@@ -1,15 +1,19 @@
 package blue.mesh;
 
+import java.io.IOException;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothServerSocket;
 import android.os.Handler;
-
+import android.util.Log;
 
 
 public class ServerThread extends Thread{
 	
+	private static final String TAG = "ServerThread";
 	private Handler handler;
     private BluetoothAdapter adapter;
     private RouterObject routerObject;
+    private BluetoothServerSocket serverSocket;
     
 	ServerThread( 
 			Handler mHandler, 
@@ -19,14 +23,26 @@ public class ServerThread extends Thread{
 		handler = mHandler;
 		adapter = mAdapter;
 		routerObject = mRouterObject;
+
+		//Attempt to listen on ServerSocket for incoming requests
+		BluetoothServerSocket tmp = null;
+
+		Log.d(TAG, "Creating SearchThread");
+
+		// Create a new listening server socket
+		try {
+			tmp = adapter.listenUsingRfcommWithServiceRecord(
+					Constants.NAME, Constants.MY_UUID_SECURE);
+		} catch (IOException e) {
+			Log.e(TAG, "listenUsingRfcommWithServiceRecord() failed", e);
+		}
+
+		serverSocket = tmp;
 	}
 	
 	public void run() {
-		//TODO: get rid of code below
-		//Using variables in code to get rid of warnings
-		handler.notify();
-		adapter.enable();
-		routerObject.notify();
+
+		
 		return;
 	}
 	
