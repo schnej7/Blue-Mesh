118a2cccb111b7e932be2d3d92c58dfa53ba229d
schnej7 <schnej7@rpi.edu>
2012-02-08 13:25:23 -0500

    Added debug constant

--- BlueMesh/src/blue/mesh/Constants.java
+++ BlueMesh/src/blue/mesh/Constants.java
@@ -8,4 +8,5 @@ public final class Constants {
     		UUID.fromString("fa87c0d0-afac-11de-8a39-0800200c9a66");
     public static final String NAME = "bluemesh";
 
+    public static final Boolean DEBUG = true;
 }
