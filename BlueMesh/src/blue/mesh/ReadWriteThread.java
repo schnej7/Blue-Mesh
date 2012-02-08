90ca2b5836dd2dbe901ab3c8b5291573f1c1e380
schnej7 <schnej7@rpi.edu>
2012-02-08 17:16:29 -0500

    Implimented constructor

--- BlueMesh/src/blue/mesh/ReadWriteThread.java
+++ BlueMesh/src/blue/mesh/ReadWriteThread.java
@@ -1,33 +1,54 @@
 package blue.mesh;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.util.Log;
 
 
 
 public class ReadWriteThread extends Thread{
-	
-	RouterObject router;
-	BluetoothSocket socket;
-	BluetoothDevice device;
-	
+
+	private final InputStream in;
+	private final OutputStream out;
+	private final RouterObject router;
+	private final BluetoothSocket socket;
+	private static final String TAG = "ReadWriteThread";
+
 	public ReadWriteThread(
 			RouterObject mRouter, 
-			BluetoothSocket mSocket, 
-			BluetoothDevice mDevice ) {
-		
+			BluetoothSocket mSocket) {
+
 		router = mRouter;
 		socket = mSocket;
-		device = mDevice;
+
+		InputStream tmpIn = null;
+		OutputStream tmpOut = null;
+
+		// Get the BluetoothSocket input and output streams
+		try {
+			tmpIn = socket.getInputStream();
+			tmpOut = socket.getOutputStream();
+		} catch (IOException e) {
+			Log.e(TAG, "temp sockets not created", e);
+		}
+
+		in = tmpIn;
+		out = tmpOut;
 	}
-	
+
 	public void run()
 	{
-		
+
 	}
-	
+
 	int write(byte [] buffer){
-		
+
+
+
 		return Constants.SUCCESS;
 	}
 }
