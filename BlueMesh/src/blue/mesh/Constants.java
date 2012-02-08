2e75cb87ee35b9f8009384545eff2a704d3009a7
schnej7 <schnej7@rpi.edu>
2012-02-08 13:02:49 -0500

    Added NAME constant

--- BlueMesh/src/blue/mesh/Constants.java
+++ BlueMesh/src/blue/mesh/Constants.java
@@ -6,5 +6,6 @@ public final class Constants {
 
     public static final UUID MY_UUID_SECURE = 
     		UUID.fromString("fa87c0d0-afac-11de-8a39-0800200c9a66");
+    public static final String NAME = "bluemesh";
 
 }
