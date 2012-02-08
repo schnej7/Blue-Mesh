5575e7ce6a2cbc5dc3ad9a048639a9090604d32d
unknown <The Omnissiah@TheOmnissiah-PC.(none)>
2012-02-07 20:17:14 -0500

    Added BeginConnection Class for RouterObject

--- BlueMesh/src/blue/mesh/RouterObject.java
+++ BlueMesh/src/blue/mesh/RouterObject.java
@@ -1,17 +1,28 @@
 package blue.mesh;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothSocket;
 import android.os.Handler;
+
 
 public class RouterObject {
 
-	Handler handler;
-	BluetoothAdapter adapter;
+	private Handler handler;
+	private BluetoothAdapter adapter;
 	
 	RouterObject( Handler mHandler, BluetoothAdapter mAdapter)
 	{
 		handler = mHandler;
 		adapter= mAdapter;
+	}
+	
+	public int BeginConnection(BluetoothSocket mSocket)
+	{
+		handler.notify();
+		adapter.enable();
+		
+		//Great Success!
+		return 1;
 	}
 }
 
