f2d71df01f171eea5db745a696d614e7944eea92
zettet <tzettersten@gmail.com>
2012-02-07 23:47:18 -0500

    Created Constants file

---
+++ BlueMesh/src/blue/mesh/Constants.java
@@ -0,0 +1,10 @@
+package blue.mesh;
+
+import java.util.UUID;
+
+public final class Constants {
+
+    public static final UUID MY_UUID_SECURE = 
+    		UUID.fromString("fa87c0d0-afac-11de-8a39-0800200c9a66");
+
+}
