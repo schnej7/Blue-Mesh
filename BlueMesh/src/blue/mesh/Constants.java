315e5a39d3fce2089d297d7bdca942208d656b5f
schnej7 <schnej7@rpi.edu>
2012-02-08 17:09:37 -0500

    Added success and failure constants Added device states

--- BlueMesh/src/blue/mesh/Constants.java
+++ BlueMesh/src/blue/mesh/Constants.java
@@ -4,6 +4,12 @@ import java.util.UUID;
 
 public final class Constants {
 
+	public static final int SUCCESS = 0;
+	public static final int FAILURE = -1;
+	
+	public static final int STATE_NONE = 1;
+	public static final int STATE_CONNECTED = 2;
+	
     public static final UUID MY_UUID = 
     		UUID.fromString("fa87c0d0-afac-11de-8a39-0800200c9a66");
     public static final String NAME = "bluemesh";
