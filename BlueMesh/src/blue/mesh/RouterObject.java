e11faeb1d86d6784c704ca78524b622d5a4610b2
unknown <The Omnissiah@TheOmnissiah-PC.(none)>
2012-02-07 20:13:10 -0500

    Added constructor for RouterObject

--- BlueMesh/src/blue/mesh/RouterObject.java
+++ BlueMesh/src/blue/mesh/RouterObject.java
@@ -1,9 +1,17 @@
 package blue.mesh;
 
-
+import android.bluetooth.BluetoothAdapter;
+import android.os.Handler;
 
 public class RouterObject {
 
+	Handler handler;
+	BluetoothAdapter adapter;
 	
+	RouterObject( Handler mHandler, BluetoothAdapter mAdapter)
+	{
+		handler = mHandler;
+		adapter= mAdapter;
+	}
 }
 
