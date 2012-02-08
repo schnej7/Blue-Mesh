aa250ffda7b1b76ae315f62f44104faa8b1e9a47
schnej7 <schnej7@rpi.edu>
2012-02-08 17:29:53 -0500

    Added max message length

--- BlueMesh/src/blue/mesh/Constants.java
+++ BlueMesh/src/blue/mesh/Constants.java
@@ -10,6 +10,8 @@ public final class Constants {
 	public static final int STATE_NONE = 1;
 	public static final int STATE_CONNECTED = 2;
 	
+	public static final int MAX_MESSAGE_LEN = 1024;
+	
     public static final UUID MY_UUID = 
     		UUID.fromString("fa87c0d0-afac-11de-8a39-0800200c9a66");
     public static final String NAME = "bluemesh";
