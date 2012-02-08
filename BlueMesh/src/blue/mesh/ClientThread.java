a65e1927aa3a8aa464ae470c9b54e8043b03dc20
unknown <The Omnissiah@TheOmnissiah-PC.(none)>
2012-02-07 20:02:48 -0500

    Added ClientThread Constructor

--- BlueMesh/src/blue/mesh/ClientThread.java
+++ BlueMesh/src/blue/mesh/ClientThread.java
@@ -1,8 +1,20 @@
 package blue.mesh;
+import android.bluetooth.BluetoothAdapter;
+import android.os.Handler;
 
 
 
 public class ClientThread {
-
+    private Handler handler;
+    private BluetoothAdapter adapter;
+    private RouterObject routerObject;
 	
-}
+	ClientThread( Handler mHandler, BluetoothAdapter mAdapter, 
+			      RouterObject mRouterObject ) {
+		handler = mHandler;
+		adapter = mAdapter;
+		routerObject = mRouterObject;
+	}
+};
+
+
