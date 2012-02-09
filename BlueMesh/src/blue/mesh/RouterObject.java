033067e8037b40da2eaf43f24177f019e6db1c0c
schnej7 <schnej7@rpi.edu>
2012-02-09 09:58:21 -0500

    Implimented 4 functions in RouterObject

--- BlueMesh/src/blue/mesh/RouterObject.java
+++ BlueMesh/src/blue/mesh/RouterObject.java
@@ -2,6 +2,7 @@ package blue.mesh;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Random;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
@@ -16,7 +17,9 @@ public class RouterObject {
 	private BluetoothAdapter adapter;
 	private List <String> connectedDevices;
 	private List <ReadWriteThread> rwThreads;
+	private List <byte[]> messageIDs;
 	private final String TAG = "RouterObject";
+	private List <byte[]> messages;
 	
 	protected RouterObject( Handler mHandler, BluetoothAdapter mAdapter) {
 		handler = mHandler;
@@ -51,23 +54,87 @@ public class RouterObject {
 		return Constants.SUCCESS;
 	}
 	
-	protected int route(byte buff[]) {
-		//Great Success!
+	protected int route(byte buffer[]) {
+		
+		byte messageID[] = new byte[Constants.MESSAGE_ID_LEN];
+		for( int i = 0; i < Constants.MESSAGE_ID_LEN; i++ ){
+			messageID[i] = buffer[i];
+		}
+		
+		//Check that the message was not received before
+		synchronized( this.messageIDs ){
+			if( messageIDs.contains(messageID) ){
+				return Constants.SUCCESS;
+			}
+			else{
+				messageIDs.add(messageID);
+				//Remove oldest message ID if too many are stored
+				if( messageIDs.size() > Constants.MSG_HISTORY_LEN){
+					messageIDs.remove(0);
+				}
+			}
+		}
+		
+		//Send the message all the threads
+		synchronized( this.rwThreads ){
+			for( ReadWriteThread aThread : rwThreads ){
+				aThread.write(buffer);
+			}
+		}
+		
+		//Add message to buffer
+		synchronized( this.messages ){
+			byte message[] = new byte[buffer.length - Constants.MESSAGE_ID_LEN];
+			for( int i = Constants.MESSAGE_ID_LEN; i < buffer.length; i++ ){
+				message[i - Constants.MESSAGE_ID_LEN] = buffer[i];
+			}
+			messages.add(buffer);
+		}
+		
 		return Constants.SUCCESS;
 	}
 	
 	protected byte [] getNextMessage() {
-		byte arr[] = null;
-		return arr;
+		if(messages.size() > 0){
+			byte message[] = messages.get(0);
+			messages.remove(0);
+			return message;
+		}
+		else{
+			return null;
+		}
 	}
 	
 	protected int getDeviceState( BluetoothDevice device ){
-		
+		synchronized( this.connectedDevices ){
+			if( connectedDevices.contains(device.getName()) ){
+				return Constants.STATE_CONNECTED;
+			}
+		}
 		return Constants.STATE_NONE;
 	}
 	
 	protected int stop() {
 		//Great Success!
+		return Constants.SUCCESS;
+	}
+	
+	protected int write( byte[] buffer ){
+		
+		Random rand = new Random();
+		byte messageID[] = new byte[Constants.MESSAGE_ID_LEN];
+		rand.nextBytes(messageID);
+		
+		byte new_buffer[] = new byte[Constants.MESSAGE_ID_LEN + buffer.length];
+		
+		for( int i = 0; i < Constants.MESSAGE_ID_LEN; i++ ){
+			new_buffer[i] = messageID[i];
+		}
+		
+		for( int i = 0; i < buffer.length; i++ ){
+			new_buffer[Constants.MESSAGE_ID_LEN + i] = buffer[i];
+		}
+		
 		return Constants.SUCCESS;
 	}
 }
