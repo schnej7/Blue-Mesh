7d35a047d2788f01f07556875735eb1247bb8752
schnej7 <schnej7@rpi.edu>
2012-02-09 09:57:39 -0500

    Added message id len Added number of messages to be stored in history

--- BlueMesh/src/blue/mesh/Constants.java
+++ BlueMesh/src/blue/mesh/Constants.java
@@ -12,6 +12,10 @@ public final class Constants {
 	
 	public static final int MAX_MESSAGE_LEN = 1024;
 	
+	public static final int MESSAGE_ID_LEN = 4;
+	
+	public static final int MSG_HISTORY_LEN = 100;
+	
     public static final UUID MY_UUID = 
     		UUID.fromString("fa87c0d0-afac-11de-8a39-0800200c9a66");
     public static final String NAME = "bluemesh";
