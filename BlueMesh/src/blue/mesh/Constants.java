4db9d4d51929af4be5f54cd47bcb80421ca99d40
schnej7 <schnej7@rpi.edu>
2012-02-10 13:11:41 -0500

    Added constants to identify the sender of a message

--- BlueMesh/src/blue/mesh/Constants.java
+++ BlueMesh/src/blue/mesh/Constants.java
@@ -16,6 +16,9 @@ public final class Constants {
 	
 	public static final int MSG_HISTORY_LEN = 100;
 	
+	public static final int SRC_ME = -1;
+	public static final int SRC_OTHER = 0;
+	
     public static final UUID MY_UUID = 
     		UUID.fromString("fa87c0d0-afac-11de-8a39-0800200c9a66");
     public static final String NAME = "bluemesh";
