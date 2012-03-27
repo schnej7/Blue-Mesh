577ea01945b1807c476b9c007cd50de7e85e80e0
schnej7 <schnej7@rpi.edu>
2012-03-27 11:48:36 -0400

    implimented quit implimented seperate thread for reading

--- WebViewTest/src/webView/test/WebViewTestActivity.java
+++ WebViewTest/src/webView/test/WebViewTestActivity.java
@@ -10,21 +10,52 @@ import android.os.Parcelable;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.Toast;
 
 public class WebViewTestActivity extends Activity {
 
-	private static final Boolean EMULATOR = true;
+	private BlueMeshServiceStub bms;
+	private Boolean TEST = true;
 	private static final String TAG = "WebViewTestActivity";
 	private ViewPager awesomePager;
 	private static int NUM_AWESOME_VIEWS = 0;
 	private Context cxt;
 	private AwesomePagerAdapter awesomeAdapter;
-	private BlueMeshServiceStub bms;
 	private Boolean stop = false;
 	private ArrayList <String> slides;
+	private ReadThread readThread;
+	
+	@Override
+	public void onDestroy(){
+		super.onDestroy();
+		readThread.interrupt();
+		bms.disconnect();
+	}
+	
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    
+    //Menu Click Event
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.quit:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
 	/** Called when the activity is first created. */
 	@Override
@@ -37,47 +68,55 @@ public class WebViewTestActivity extends Activity {
 		awesomePager = (ViewPager) findViewById(R.id.awesomepager);
 		awesomePager.setAdapter(awesomeAdapter);
 		slides = new ArrayList<String>();
-	}
-
-	public void onStart(){
-		super.onStart();
-
-
-		if( !EMULATOR ){
-			try{
-				bms = new BlueMeshServiceStub();
-			}
-			catch(NullPointerException e){
-				Toast.makeText(cxt, "Bluetooth Not Enabeled", Toast.LENGTH_LONG).show();
-				Log.e(TAG, "BlueMeshService Constructor failed");
-				return;
-			}
-		}
-		else{
+		
+		try{
 			bms = new BlueMeshServiceStub();
+		}
+		catch(NullPointerException e){
+			Toast.makeText(cxt, "Bluetooth Not Enabeled", Toast.LENGTH_LONG).show();
+			Log.e(TAG, "BlueMeshService Constructor failed");
+			return;
 		}
 		
 		slides.add("<html><body><marquee>WELCOME</marquee></body></html>");
 		NUM_AWESOME_VIEWS++;
 		awesomeAdapter.notifyDataSetChanged();
 		
+		bms.launch();
+	}
 
-		while (true){
-			if(stop){
-				Log.d(TAG, "readThread interrupted");
-				return;
-			}
-			byte bytes[] = null;
-			bytes = bms.pull();
-			if( bytes == null){
-				//We got nothing
-			}
-			else{
-				if( EMULATOR && NUM_AWESOME_VIEWS > 10 ){ stop = true; }
-				slides.add(new String(bytes) + NUM_AWESOME_VIEWS);
-				awesomeAdapter.notifyDataSetChanged();
-				NUM_AWESOME_VIEWS++;
-				awesomePager.setCurrentItem(NUM_AWESOME_VIEWS - 1);
+	public void onStart(){
+		super.onStart();		
+		readThread = new ReadThread();
+		readThread.start();
+	}
+	
+	private void addSlide(byte[] bytes){
+		slides.add(new String(bytes) + NUM_AWESOME_VIEWS);
+		awesomeAdapter.notifyDataSetChanged();
+		NUM_AWESOME_VIEWS++;
+		awesomePager.setCurrentItem(NUM_AWESOME_VIEWS - 1);
+	}
+	
+	private class ReadThread extends Thread {
+		public void run(){
+			while (true){
+				if( this.isInterrupted()){
+					stop = true;
+				}
+				if(stop){
+					Log.d(TAG, "readThread interrupted");
+					return;
+				}
+				byte bytes[] = null;
+				bytes = bms.pull();
+				if( bytes == null){
+					//We got nothing
+				}
+				else{
+					if( TEST && NUM_AWESOME_VIEWS > 10 ){ stop = true; }
+					addSlide(bytes);
+				}
 			}
 		}
 	}
